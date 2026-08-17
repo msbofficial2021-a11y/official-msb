4ab2aab1492b6ef31ac8ae8eb28e32c596d76ae9
msbofficial2021-a11y <msb.official2021@gmail.com>
2026-08-17 16:25:04 +0900

    CSSファイルの管理構成を追加

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -12,13 +12,67 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /**
  * CSSを読み込みます。
+ *
+ * 読み込み順は重要です。
+ * 先にリセット・基本設計を読み込み、その後にレイアウト・コンポーネント・ページ固有CSSを読み込みます。
  */
 function official_msb_enqueue_assets() {
+	$theme_version = wp_get_theme()->get( 'Version' );
+
 	wp_enqueue_style(
 		'official-msb-style',
 		get_stylesheet_uri(),
 		array(),
-		wp_get_theme()->get( 'Version' )
+		$theme_version
+	);
+
+	wp_enqueue_style(
+		'official-msb-reset',
+		get_theme_file_uri( '/assets/css/base/reset.css' ),
+		array( 'official-msb-style' ),
+		$theme_version
+	);
+
+	wp_enqueue_style(
+		'official-msb-foundation',
+		get_theme_file_uri( '/assets/css/base/foundation.css' ),
+		array( 'official-msb-reset' ),
+		$theme_version
+	);
+
+	wp_enqueue_style(
+		'official-msb-container',
+		get_theme_file_uri( '/assets/css/layout/container.css' ),
+		array( 'official-msb-foundation' ),
+		$theme_version
+	);
+
+	wp_enqueue_style(
+		'official-msb-header',
+		get_theme_file_uri( '/assets/css/layout/header.css' ),
+		array( 'official-msb-container' ),
+		$theme_version
+	);
+
+	wp_enqueue_style(
+		'official-msb-footer',
+		get_theme_file_uri( '/assets/css/layout/footer.css' ),
+		array( 'official-msb-header' ),
+		$theme_version
+	);
+
+	wp_enqueue_style(
+		'official-msb-button',
+		get_theme_file_uri( '/assets/css/components/button.css' ),
+		array( 'official-msb-footer' ),
+		$theme_version
+	);
+
+	wp_enqueue_style(
+		'official-msb-home',
+		get_theme_file_uri( '/assets/css/pages/home.css' ),
+		array( 'official-msb-button' ),
+		$theme_version
 	);
 }
 add_action( 'wp_enqueue_scripts', 'official_msb_enqueue_assets' );
